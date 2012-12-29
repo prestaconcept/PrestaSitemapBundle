1144a7657b9928a8d9e17370445a0e0a14f4da9d
Andrej Hudec <pulzarraider2@gmail.com>
2012-12-29 10:17:11 +0100

    Encode urls with ampersand

--- Tests/Sitemap/Url/UrlConcreteTest.php
+++ Tests/Sitemap/Url/UrlConcreteTest.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 
 namespace Presta\SitemapBundle\Test\Sitemap\Url;
 
@@ -10,25 +10,34 @@ use Presta\SitemapBundle\Sitemap\Url\UrlConcrete;
  */
 class UrlConcreteTest extends \PHPUnit_Framework_TestCase
 {
-    protected  $url;
-            
-    protected function setUp()
+    /**
+     * @dataProvider testToXmlProvider
+     */
+    public function testToXml($expectedXml, $loc, $lastmod = null, $changefreq = null, $priority = null)
     {
-        $this->url = new UrlConcrete('http://acme.com/');
+        $url = new UrlConcrete($loc, $lastmod, $changefreq, $priority);
+        $this->assertEquals($expectedXml, $url->toXml());
     }
-    
-    public function testToXml()
+
+    public function testToXmlProvider()
     {
-        $xml = new \DOMDocument;
-        
-        $str = '<?xml version="1.0" encoding="UTF-8"?><urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9"';
-        $str .= '>' . $this->url->toXml() . '</urlset>';
-        
-        $xml->loadXML($str);
-        
-        
-        $this->assertEquals(1, $xml->getElementsByTagName('url')->length);
+        return array(
+            array('<url><loc>http://example.com/</loc></url>', 'http://example.com/'),
+            array('<url><loc>http://example.com/abcd</loc></url>', 'http://example.com/abcd'),
+            array('<url><loc>http://example.com/abcd/?a=1&amp;b=cdf</loc></url>', 'http://example.com/abcd/?a=1&b=cdf'),
+            array('<url><loc>http://example.com/</loc><lastmod>2012-12-29T10:39:12+01:00</lastmod></url>', 'http://example.com/', new \DateTime('2012-12-29 10:39:12')),
+            array('<url><loc>http://example.com/</loc><changefreq>always</changefreq></url>', 'http://example.com/', null, 'always'),
+            array('<url><loc>http://example.com/</loc><changefreq>hourly</changefreq></url>', 'http://example.com/', null, 'hourly'),
+            array('<url><loc>http://example.com/</loc><changefreq>weekly</changefreq></url>', 'http://example.com/', null, 'weekly'),
+            array('<url><loc>http://example.com/</loc><changefreq>monthly</changefreq></url>', 'http://example.com/', null, 'monthly'),
+            array('<url><loc>http://example.com/</loc><changefreq>yearly</changefreq></url>', 'http://example.com/', null, 'yearly'),
+            array('<url><loc>http://example.com/</loc><changefreq>never</changefreq></url>', 'http://example.com/', null, 'never'),
+            array('<url><loc>http://example.com/</loc><changefreq>daily</changefreq></url>', 'http://example.com/', null, 'daily'),
+            array('<url><loc>http://example.com/</loc><priority>0.1</priority></url>', 'http://example.com/', null, null, 0.1),
+            array('<url><loc>http://example.com/</loc><priority>0.5</priority></url>', 'http://example.com/', null, null, 0.5),
+            array('<url><loc>http://example.com/</loc><priority>1.0</priority></url>', 'http://example.com/', null, null, 1),
+            array('<url><loc>http://example.com/abcd/?a=1&amp;b=cdf&amp;ghj=ijklmn</loc><lastmod>2012-01-01T00:00:00+01:00</lastmod><changefreq>daily</changefreq><priority>0.7</priority></url>', 'http://example.com/abcd/?a=1&b=cdf&ghj=ijklmn', new \DateTime('2012-1-1 00:00:00'), 'daily', 0.7),
+            array('<url><loc>http://example.com/abcd/?a=1&amp;b=cdf&amp;ghj=ijklmn</loc><changefreq>daily</changefreq><priority>0.7</priority></url>', 'http://example.com/abcd/?a=1&b=cdf&ghj=ijklmn', null, 'daily', 0.7),
+        );
     }
-    
-    
 }
