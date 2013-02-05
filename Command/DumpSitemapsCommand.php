0b80349b7ec635d4418f15df931bfe23cafa29a0
Alex Vasilenko <aa.vasilenko@gmail.com>
2013-02-05 15:11:34 +0200

    added missing namespace for request

--- Command/DumpSitemapsCommand.php
+++ Command/DumpSitemapsCommand.php
@@ -15,6 +15,7 @@ use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\HttpFoundation\Request;
 
 /**
  * Command to dump the sitemaps to provided directory
