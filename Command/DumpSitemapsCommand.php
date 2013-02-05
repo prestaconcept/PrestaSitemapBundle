85ddc35afc86a9ca5223d08b35476e704b15ee77
Alex Vasilenko <aa.vasilenko@gmail.com>
2013-02-05 15:06:30 +0200

    emulate request scope at dumper

--- Command/DumpSitemapsCommand.php
+++ Command/DumpSitemapsCommand.php
@@ -68,9 +68,10 @@ class DumpSitemapsCommand extends ContainerAwareCommand
 
         // Set Router's host used for generating URLs from configuration param
         // There is no other way to manage domain in CLI
-        $this->getContainer()->get('router')->getContext()->setHost(
+        $this->getContainer()->set('request', Request::create(
             parse_url($this->getContainer()->getParameter('presta_sitemap.dumper_base_url'), PHP_URL_HOST)
-        );
+        ));
+        $this->getContainer()->enterScope('request');
 
         if ($input->getOption('section')) {
             $output->writeln(
