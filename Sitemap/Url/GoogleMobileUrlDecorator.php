0018365cd449b7313080071a85db9f85728d95f1
Yann Eugoné <yann.eugone@gmail.com>
2016-09-29 21:34:58 +0200

    Uniformize files copyright (#133)

--- Sitemap/Url/GoogleMobileUrlDecorator.php
+++ Sitemap/Url/GoogleMobileUrlDecorator.php
@@ -1,7 +1,7 @@
 <?php
 
 /**
- * This file is part of the PrestaSitemapBundle
+ * This file is part of the PrestaSitemapBundle package.
  *
  * (c) PrestaConcept <www.prestaconcept.net>
  *
--- Tests/bootstrap.php
+++ Tests/bootstrap.php
@@ -1,7 +1,9 @@
 <?php
-/*
- * This file is part of the prestaSitemapPlugin package.
- * (c) David Epely <depely@prestaconcept.net>
+
+/**
+ * This file is part of the PrestaSitemapBundle package.
+ *
+ * (c) PrestaConcept <www.prestaconcept.net>
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
