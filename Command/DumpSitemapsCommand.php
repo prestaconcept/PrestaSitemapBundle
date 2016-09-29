6ee877831265cbba3e5727fe19a3257f1bc89154
Yann Eugoné <yann.eugone@gmail.com>
2016-09-29 20:48:49 +0200

    Uniformize files copyright

--- Command/DumpSitemapsCommand.php
+++ Command/DumpSitemapsCommand.php
@@ -1,8 +1,9 @@
 <?php
 
-/*
+/**
  * This file is part of the PrestaSitemapBundle package.
- * (c) David Epely <depely@prestaconcept.net>
+ *
+ * (c) PrestaConcept <www.prestaconcept.net>
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
