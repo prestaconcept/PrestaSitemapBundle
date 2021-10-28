a81de00e5496e4284f63688cebc41f9887e52d38
Kevin Bond <kevinbond@gmail.com>
2021-10-28 08:47:37 -0400

    fix self deprecation

--- tests/Unit/Service/DumperTest.php
+++ tests/Unit/Service/DumperTest.php
@@ -18,6 +18,8 @@ use Presta\SitemapBundle\Service\Dumper;
 use Presta\SitemapBundle\Sitemap\Url\UrlConcrete;
 use Symfony\Component\EventDispatcher\EventDispatcher;
 use Symfony\Component\Filesystem\Filesystem;
+use Symfony\Component\Routing\Loader\ClosureLoader;
+use Symfony\Component\Routing\Router;
 use Throwable;
 
 class DumperTest extends TestCase
@@ -39,6 +41,11 @@ class DumperTest extends TestCase
      */
     private $dumper;
 
+    /**
+     * @var Router
+     */
+    private $router;
+
     public function setUp(): void
     {
         self::removeDir();
@@ -46,7 +53,8 @@ class DumperTest extends TestCase
 
         $this->eventDispatcher = new EventDispatcher();
         $this->filesystem = new Filesystem();
-        $this->dumper = new Dumper($this->eventDispatcher, $this->filesystem, 'sitemap', 5);
+        $this->router = new Router(new ClosureLoader(), null);
+        $this->dumper = new Dumper($this->eventDispatcher, $this->filesystem, 'sitemap', 5, $this->router);
 
         (new Filesystem())->remove(\glob(sys_get_temp_dir() . '/PrestaSitemaps-*'));
     }
